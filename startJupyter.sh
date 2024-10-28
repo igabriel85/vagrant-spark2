2cbe3cc8234879dbd2038915ab1e0e7e7e750c35
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2024-10-28 19:06:51 +0200

    updated jupyter start script

--- startJupyter.sh
+++ startJupyter.sh
@@ -3,4 +3,4 @@ export PATH=$CONDA_DIR/bin:$PATH
 export SPARK_HOME=/usr/local/spark
 export PATH=$PATH:${SPARK_HOME}/bin
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip
-jupyter notebook --NotebookApp.token='bd2022' --ip=0.0.0.0 --port=8888 --no-browser --notebook-dir='/home/vagrant' &
+jupyter lab --NotebookApp.token='bd2024' --ip=0.0.0.0 --port=8888 --no-browser --notebook-dir='/home/vagrant' &
