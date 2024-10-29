4850d1eb3c530a4fe8b2fa3eb5c358f0c33094f1
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2024-10-29 12:41:16 +0200

    updated jupyter startup script to include log and pid file

--- startJupyter.sh
+++ startJupyter.sh
@@ -3,4 +3,6 @@ export PATH=$CONDA_DIR/bin:$PATH
 export SPARK_HOME=/usr/local/spark
 export PATH=$PATH:${SPARK_HOME}/bin
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip
-jupyter lab --NotebookApp.token='bd2024' --ip=0.0.0.0 --port=8888 --no-browser --notebook-dir='/home/vagrant' &
+
+# start jupyter and log to /var/log/jupyter.log add pid to /var/run/jupyter.pid
+jupyter lab  --NotebookApp.token='bd2024' --ip=0.0.0.0 --port=8888 --no-browser --notebook-dir='/home/vagrant' > /var/log/jupyter.log 2>&1 & echo $! > /var/run/jupyter.pid
